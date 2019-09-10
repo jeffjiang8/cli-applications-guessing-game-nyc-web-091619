e83d9c5873ac33112117d6449caef9ad331f649d
Jinfeng Jiang <jeffjiang8@gmail.com>
2019-09-10 21:02:27 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -2,4 +2,11 @@
 def run_guessing_game
   computer_guess = rand(1..6)
   user_guess = gets.chomp.strip
-  
+  if user_guess = computer_guess
+    puts "You guessed the correct number!"
+  else 
+    puts "Sorry! The computer guessd <#{computer_guess}>."
+  else user_guess = "exit"
+    puts 'Goodbye!'
+  end 
+end 
