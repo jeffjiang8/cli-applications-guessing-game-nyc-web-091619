d680d79701ea5825dbda4f5bdac51d38ea00b09c
Jinfeng Jiang <jeffjiang8@gmail.com>
2019-09-10 21:02:55 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -4,7 +4,7 @@ def run_guessing_game
   user_guess = gets.chomp.strip
   if user_guess = computer_guess
     puts "You guessed the correct number!"
-  else 
+  elsif 
     puts "Sorry! The computer guessd <#{computer_guess}>."
   else user_guess = "exit"
     puts 'Goodbye!'
