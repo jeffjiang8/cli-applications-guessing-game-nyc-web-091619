b91ed92f2bc580bf2fa6d4199a1f6c61e42015d2
Jinfeng Jiang <jeffjiang8@gmail.com>
2019-09-10 21:17:06 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,7 +1,6 @@
 # Code your solution here!
 def run_guessing_game
   computer_guess = rand(6) + 1 
-  
   user_guess = gets.chomp
   if user_guess == computer_guess.to_s
     puts "You guessed the correct number!"
