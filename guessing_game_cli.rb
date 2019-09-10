c99e71ccb281dc7dacab3854adff963a331be899
Jinfeng Jiang <jeffjiang8@gmail.com>
2019-09-10 21:09:58 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -2,7 +2,7 @@
 def run_guessing_game
   computer_guess = rand(1..6)
   puts "Guess a number between 1 and 6:"
-  user_guess = gets.chomp.strip
+  user_guess = gets.chomp
   if user_guess == computer_guess
     puts "You guessed the correct number!"
   elsif user_guess == "exit"
